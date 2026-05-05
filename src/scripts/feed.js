6787c620f95bc705b607a3523beabcfc52f749a8
Lucas Moraes <lucas.moraes@sptech.school>
2026-05-05 15:01:28 -0300

    Feat: Adição de icone no usuario, e inversão de ordem posts

--- src/scripts/feed.js
+++ src/scripts/feed.js
@@ -1,71 +1,80 @@
+user_name_sidebar.innerHTML = sessionStorage.NOME_USUARIO;
+
 function limparFormulario() {
-    ipt_title_post.value = ""
-    ipt_content_post = ""
+	ipt_title_post.value = "";
+	ipt_content_post = "";
 }
 
-
 function publicar() {
-    var idUsuario = sessionStorage.ID_USUARIO;
+	var idUsuario = sessionStorage.ID_USUARIO;
 
-    var corpo = {
-        titulo: ipt_title_post.value,
-        descricao: ipt_content_post.value
-    }
+	var corpo = {
+		titulo: ipt_title_post.value,
+		descricao: ipt_content_post.value,
+	};
 
-    fetch(`/post/publish/${idUsuario}`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(corpo)
-    }).then(function (resposta) {
+	fetch(`/post/publish/${idUsuario}`, {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(corpo),
+	})
+		.then(function (resposta) {
+			console.log("resposta: ", resposta);
 
-        console.log("resposta: ", resposta);
+			if (resposta.ok) {
+				limparFormulario();
+				atualizarFeed();
+				location.reload();
+			} else if (resposta.status == 404) {
+				window.alert("Deu 404!");
+			} else {
+				throw (
+					"Houve um erro ao tentar realizar a postagem! Código da resposta: " +
+					resposta.status
+				);
+			}
+		})
+		.catch(function (resposta) {
+			console.log(`#ERRO: ${resposta}`);
+		});
 
-        if (resposta.ok) {
-            window.alert("Post realizado com sucesso pelo usuario de ID: " + idUsuario + "!");
-            limparFormulario();
-            atualizarFeed();
-        } else if (resposta.status == 404) {
-            window.alert("Deu 404!");
-        } else {
-            throw ("Houve um erro ao tentar realizar a postagem! Código da resposta: " + resposta.status);
-        }
-    }).catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`);
-    });
-
-    return false;
+	return false;
 }
 
 function atualizarFeed() {
-    console.log('Atualizando')
-    fetch("/post/list").then(function (resposta) {
-        if (resposta.ok) {
-            if (resposta.status == 204) {
-                alert("Nenhum resultado encontrado.");
-                throw "Nenhum resultado encontrado!!";
-            }
-            
-            resposta.json().then(function (respostaJson) {
-                var feed = document.getElementById("feed_container");
-                feed.innerHTML = ""; 
+	console.log("Atualizando");
+	fetch("/post/list")
+		.then(function (resposta) {
+			if (resposta.ok) {
+				if (resposta.status == 204) {
+					alert("Nenhum resultado encontrado.");
+					throw "Nenhum resultado encontrado!!";
+				}
 
-                for (let i = 0; i < respostaJson.length; i++) {
-                    var publicacao = respostaJson[i];
+				resposta.json().then(function (respostaJson) {
+					var feed = document.getElementById("feed_container");
+					feed.innerHTML = "";
 
-                    var dataObj = new Date(publicacao.criado_em);
-                    var dataFormatada = dataObj.toLocaleDateString('pt-BR', {
-                        day: 'numeric', 
-                        month: 'short', 
-                        year: 'numeric'
-                    }) + ' · ' + dataObj.toLocaleTimeString('pt-BR', {
-                        hour: '2-digit', 
-                        minute: '2-digit'
-                    });
+					for (let i = respostaJson.length - 1; i >= 0; i--) {
+						var publicacao = respostaJson[i];
 
-                    //TODO: CRIAR UMA FUNÇÂO PARA LISTAR OS COMENTARIOS
-                    feed.innerHTML += `
+						var dataObj = new Date(publicacao.criado_em);
+						var dataFormatada =
+							dataObj.toLocaleDateString("pt-BR", {
+								day: "numeric",
+								month: "short",
+								year: "numeric",
+							}) +
+							" · " +
+							dataObj.toLocaleTimeString("pt-BR", {
+								hour: "2-digit",
+								minute: "2-digit",
+							});
+
+						//TODO: CRIAR UMA FUNÇÂO PARA LISTAR OS COMENTARIOS
+						feed.innerHTML += `
                         <div class="post">
                             <div class="post-meta">
                                 <span class="post-author">${publicacao.nome}</span>
@@ -86,14 +95,15 @@ function atualizarFeed() {
                             </div>
                         </div>
                     `;
-                }
-            });
-        } else {
-            throw ('Houve um erro na API!');
-        }
-    }).catch(function (erro) {
-        console.error(erro);
-    });
+					}
+				});
+			} else {
+				throw "Houve um erro na API!";
+			}
+		})
+		.catch(function (erro) {
+			console.error(erro);
+		});
 }
 
 atualizarFeed();
